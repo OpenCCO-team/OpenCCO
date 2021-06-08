755992d09f14bd5926fdd20b86d0e9ac829c39bf
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2021-06-08 16:45:50 +0200

    export example

--- Kamiya_s_Algorithm/testKamiya.cpp
+++ Kamiya_s_Algorithm/testKamiya.cpp
@@ -11,9 +11,16 @@ using ceres::CostFunction;
 using ceres::Problem;
 using ceres::Solve;
 using ceres::Solver;
+
+#include "DGtal/io/boards/Board2D.h"
+
+using namespace DGtal;
+using namespace DGtal::Z2i;
+
 #include "CLI11.hpp"
 #include "DGtal/base/Common.h"
 #include "DGtal/helpers/StdDefs.h"
+
 
 
 
@@ -45,9 +52,9 @@ struct F28a {
   template <typename T>
   bool operator()(const T* const x1, const T* const x2, T* residual) const {
     residual[0] =  (deltap1/k)*x1[0]*x1[0]*pow((f0*(((x1[0]*x1[0]*x1[0])/f1)+(x2[0]*x2[0]*x2[0])/f2)), 2.0/3.0)
-                 - (f0*l0*x1[0]*x1[0]) - f1 * l1 * pow(f0*((x1[0]*x1[0]*x1[0]/f1)+(x2[0]*x2[0]*x2[0])/f2), 2.0/3.0);
+    - (f0*l0*x1[0]*x1[0]) - f1 * l1 * pow(f0*((x1[0]*x1[0]*x1[0]/f1)+(x2[0]*x2[0]*x2[0])/f2), 2.0/3.0);
     residual[1] =  (deltap2/k)*x2[0]*x2[0]*pow((f0*(((x1[0]*x1[0]*x1[0])/f1)+(x2[0]*x2[0]*x2[0])/f2)), 2.0/3.0)
-                 - (f0*l0*x2[0]*x2[0]) - f2 * l2 * pow(f0*((x1[0]*x1[0]*x1[0]/f1)+(x2[0]*x2[0]*x2[0])/f2), 2.0/3.0);
+    - (f0*l0*x2[0]*x2[0]) - f2 * l2 * pow(f0*((x1[0]*x1[0]*x1[0]/f1)+(x2[0]*x2[0]*x2[0])/f2), 2.0/3.0);
     
     return true;
   }
@@ -86,21 +93,20 @@ static void kamiyaOpt(double deltaP1, double deltaP2, double f0, double f1, doub
 
 int main(int argc, char** argv) {
   // parse command line CLI ----------------------------------------------
-   CLI::App app;
+  CLI::App app;
   double x0 {0};
   double y0 {0};
   
   double x1 {0};
   double y1 {0};
-
+  
   double x2 {0};
   double y2 {0};
-
+  
   double gamma {3.0};
   double k {8*3.6/M_PI};
   double r_ori {1.0};
-  
-  
+  std::string file_plot {"res.gnuplot"};
   
   app.description("Test the Kamiya algorithm\n");
   app.add_option("--x0", x0, "x0", true );
@@ -111,44 +117,44 @@ int main(int argc, char** argv) {
   
   app.add_option("--x2", x2, "x2", true );
   app.add_option("--y2", y2, "y2", true );
-
-
   
   app.add_option("-k", k, "k ", true );
   app.add_option("-r", r_ori, "r_ori", true );
   app.add_option("-g", gamma, "gamma", true );
-
   
-
-   app.get_formatter()->column_width(40);
-   CLI11_PARSE(app, argc, argv);
-   // END parse command line using CLI ----------------------------------------------
-
+  app.add_option("--genGnuPlot", file_plot, "generate a gnuplot to display the result.", true );
+  
+  
+  
+  app.get_formatter()->column_width(40);
+  CLI11_PARSE(app, argc, argv);
+  // END parse command line using CLI ----------------------------------------------
+  
   
   
   // Prepare
   DGtal::Z2i::RealPoint p0 (x0, y0);
   DGtal::Z2i::RealPoint p1 (x1, y1);
   DGtal::Z2i::RealPoint p2 (x2, y2);
- 
+  
   double r0 = r_ori;
   double r1 = r_ori;
   double r2 = r_ori;
-
+  
   double f0 = k * r0*r0*r0;
-    double f1 = 0.5*f0;//k * r1*r1*r1;
-    double f2 = 0.5*f0;//k * r2*r2*r2;
+  double f1 = 0.5*f0;//k * r1*r1*r1;
+  double f2 = 0.5*f0;//k * r2*r2*r2;
   // Starting position from Equation (21) for initialisation as mentionned page 11 [Clara Jaquet et HT]
   //DGtal::Z2i::RealPoint pb ((f0*x0+f1*x1+f2*x2)/(f0+f1+f2), (f0*y0+f1*y1+f2*y2)/(f0+f1+f2));
-    DGtal::Z2i::RealPoint pb ((f0*x0+f1*x1+f2*x2)/(2.0*f0), (f0*y0+f1*y1+f2*y2)/(2.0*f0));
-    std::cout<<"Init:"<<pb<<std::endl;
+  DGtal::Z2i::RealPoint pb ((f0*x0+f1*x1+f2*x2)/(2.0*f0), (f0*y0+f1*y1+f2*y2)/(2.0*f0));
+  std::cout<<"Init:"<<pb<<std::endl;
   double l0 = (p0 - pb).norm();
   double l1 = (p1 - pb).norm();
   double l2 = (p2 - pb).norm();
-    std::cout<<"l0="<<l0<<", l1="<<l1<<", l2="<<l2<<std::endl;
+  std::cout<<"l0="<<l0<<", l1="<<l1<<", l2="<<l2<<std::endl;
   double deltaP1 = k*((f0*l0)/(r0*r0)+(f1*l1)/(r1*r1));
   double deltaP2 = k*((f0*l0)/(r0*r0)+(f2*l2)/(r2*r2));
-
+  
   double rr1 = r_ori;
   double rr2 = r_ori;
   // The variable to solve for with its initial value. It will be
@@ -166,15 +172,15 @@ int main(int argc, char** argv) {
     ///f1 = k * rr1*rr1*rr1;
     ///f2 = k * rr2*rr2*rr2;
     //r0 = pow((pow(rr1, gamma) + pow(rr2, gamma)), 1.0/gamma);
-      //Equation 27
-      r0 = pow(f0*(pow(rr1, gamma)/f1 + pow(rr2, gamma)/f2), 1.0/gamma);
+    //Equation 27
+    r0 = pow(f0*(pow(rr1, gamma)/f1 + pow(rr2, gamma)/f2), 1.0/gamma);
     ///f0 = k * r0*r0*r0;
     // Equation (26) page 13
     //DGtal::Z2i::RealPoint pbNew ((f0*x0+f1*x1+f2*x2)/(2.0*f0), (f0*y0+f1*y1+f2*y2)/(2.0*f0));
     ///pb[0] = (x0*r0*r0/l0 + x1*r1*r1/l1 + x2*r2*r2/l2)/(r0*r0/l0+r1*r1/l1+r2*r2/l2);
     ///pb[1] = (y0*r0*r0/l0 + y1*r1*r1/l1 + y2*r2*r2/l2)/(r0*r0/l0+r1*r1/l1+r2*r2/l2);
-      pb[0] = (x0*r0/l0 + x1*r1/l1 + x2*r2/l2)/(r0/l0+r1/l1+r2/l2);
-      pb[1] = (y0*r0/l0 + y1*r1/l1 + y2*r2/l2)/(r0/l0+r1/l1+r2/l2);
+    pb[0] = (x0*r0/l0 + x1*r1/l1 + x2*r2/l2)/(r0/l0+r1/l1+r2/l2);
+    pb[1] = (y0*r0/l0 + y1*r1/l1 + y2*r2/l2)/(r0/l0+r1/l1+r2/l2);
     deltaP1 = k*((f0*l0)/(r0*r0)+(f1*l1)/(r1*r1));
     deltaP2 = k*((f0*l0)/(r0*r0)+(f2*l2)/(r2*r2));
     std::cout << "xx1 : " << rr1 << " and xx2 " << rr2 << " r0 " << r0 << "\n";
@@ -183,12 +189,47 @@ int main(int argc, char** argv) {
   
   f1 = k * rr1*rr1*rr1;
   f2 = k * rr2*rr2*rr2;
-  r0 = pow(pow(rr1, gamma) + pow(rr2, gamma), 1.0/gamma);
+  r0 = pow(f0*(pow(rr1, gamma)/f1 + pow(rr2, gamma)/f2), 1.0/gamma);
+
+//  r0 = pow(pow(rr1, gamma) + pow(rr2, gamma), 1.0/gamma);
   f0 = k * r0*r0*r0;
   
- // DGtal::Z2i::RealPoint pbNew ((f0*x0+f1*x1+f2*x2)/(2.0*f0), (f0*y0+f1*y1+f2*y2)/(2.0*f0));
-
+  // DGtal::Z2i::RealPoint pbNew ((f0*x0+f1*x1+f2*x2)/(2.0*f0), (f0*y0+f1*y1+f2*y2)/(2.0*f0));
+  
   std::cout << "xx1 : " << rr1 << " and xx2 " << rr2 << " r0 " << r0 << "\n";
   std::cout << "pbNew[0]  : " << pb[0] << " and pbNew[1] " << pb[1] << "\n";
+  Board2D board;
+  // Draw initial points
+  board.setFillColor(DGtal::Color::Red);
+  board.drawCircle(p0[0], p0[1], r_ori, 1);
+  board.drawCircle(p1[0], p1[1], r_ori, 1);
+  board.drawCircle(p2[0], p2[1], r_ori, 1);
+  board.setFillColor(DGtal::Color::Yellow);
+  board.drawCircle(pbInit[0], pbInit[1], r_ori, 1);
+  // Draw initial lines
+  board.setPenColor(DGtal::Color(200, 200, 0, 100));
+  board.setLineWidth(57.5*r_ori);
+  board.drawLine(p0[0], p0[1], pbInit[0], pbInit[1],2);
+  board.drawLine(p1[0], p1[1], pbInit[0], pbInit[1],2);
+  board.drawLine(p2[0], p2[1], pbInit[0], pbInit[1],2);
+  // Draw results
+  board.setLineWidth(0);
+  board.setFillColor(DGtal::Color::Green);
+  board.drawCircle(pb[0], pb[1], r0, 0);
+  // Draw initial lines
+  board.setPenColor(DGtal::Color(20, 200, 200, 100));
+  board.setLineWidth(57.5*r0);
+  board.drawLine(p0[0], p0[1], pb[0], pb[1],2);
+  board.setLineWidth(57.5*sqrt(rr1));
+  board.drawLine(p1[0], p1[1], pb[0], pb[1],2);
+  board.setLineWidth(57.5*sqrt(rr2  ));
+  board.drawLine(p2[0], p2[1], pb[0], pb[1],2);
+
+  
+  
+  board.saveEPS("logoDGtal.eps");
+  board.saveSVG("logoDGtal.svg");
+
+  
   return 0;
 }
