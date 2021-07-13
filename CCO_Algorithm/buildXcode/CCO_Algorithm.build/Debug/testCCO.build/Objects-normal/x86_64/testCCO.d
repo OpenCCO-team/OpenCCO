a76e66b9fc83afc6abb1370b4e9f58bb22927f88
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2021-07-13 16:22:13 +0200

    remove buildXcode

--- CCO_Algorithm/buildXcode/CCO_Algorithm.build/Debug/testCCO.build/Objects-normal/x86_64/testCCO.d
+++
@@ -1,3 +0,0 @@
-dependencies: /Users/kerautre/EnCours/LiverCCO/CCO_Algorithm/testCCO.cpp \
-  /Users/kerautre/EnCours/LiverCCO/base/CoronaryArteryTree.h \
-  /Users/kerautre/EnCours/LiverCCO/helpers/geomhelpers.h
--- CCO_Algorithm/buildXcode/CMakeFiles/3.19.7/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/kerautre/EnCours/LiverCCO/CCO_Algorithm/buildXcode/CMakeFiles/3.19.7/CompilerIdCXX/CMakeCXXCompilerId.cpp
