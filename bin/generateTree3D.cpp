85aba8aa4213069a89ca74730f43e6df9f4e0a7a
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2022-04-18 12:24:52 +0200

    export mesh by default

--- bin/generateTree3D.cpp
+++ bin/generateTree3D.cpp
@@ -5,6 +5,7 @@
 
 #include "DGtal/base/Common.h"
 #include "DGtal/helpers/StdDefs.h"
+#include "DGtal/shapes/Mesh.h"
 
 #include "CLI11.hpp"
 
@@ -12,8 +13,9 @@
 #include "GeomHelpers.h"
 #include "ConstructionHelpers.h"
 
+#ifdef WITH_VISU3D_QGLVIEWER
 #include "DGtal/io/viewers/Viewer3D.h"
-
+#endif
 /**
  * @brief main function call
  *
@@ -26,14 +28,21 @@ int main(int argc, char **argv)
   
   // parse command line using CLI ----------------------------------------------
   CLI::App app;
+  app.description("Generated a 3D tree using the CCO algorithm. By default it generates a 3D mesh.");
   int nbTerm {500};
   double aPerf {20000};
   bool verbose {false};
-  std::string nameImgDom {""}; 
+  bool display3D {false};
+  std::string nameImgDom {""};
+  std::string outputMeshName {"result.off"};
   std::vector<int> postInitV {-1,-1};
   app.add_option("-n,--nbTerm,1", nbTerm, "Set the number of terminal segments.", true);
   app.add_option("-a,--aPerf,2", aPerf, "The value of the input parameter A perfusion.", true);
   app.add_option("--organDomain,-d", nameImgDom, "Define the organ domain using a mask image (organ=255).");
+  app.add_option("-o,--outputName",outputMeshName, "Output the 3D mesh", true);
+#ifdef WITH_VISU3D_QGLVIEWER
+  app.add_flag("--view", display3D, "display 3D view using QGLViewer");
+#endif
   auto pInit = app.add_option("-p,--posInit", postInitV, "Initial position of root, if not given the position of point is determined from the image center")
   ->expected(2);
   app.add_flag("-v,--verbose", verbose);
@@ -56,41 +65,69 @@ int main(int argc, char **argv)
   }
   end = clock();
   printf ("Execution time: %0.8f sec\n", ((double) end - start)/CLOCKS_PER_SEC);
-
-  typedef DGtal::Viewer3D<> MyViewer;
-  MyViewer viewer;
-  viewer.show();
+  
+  
+#ifdef WITH_VISU3D_QGLVIEWER
+  if (display3D){
+    typedef DGtal::Viewer3D<> MyViewer;
+    MyViewer viewer;
+    viewer.show();
+    unsigned int i = 0;
+    double thickness = 1;
+    viewer << DGtal::CustomColors3D(DGtal::Color(0,0,250),DGtal::Color(0,0,250));
+    DGtal::Z3i::RealPoint p1 = tree.myVectSegments[1].myCoordinate;
+    DGtal::Z3i::RealPoint p2 = tree.myVectSegments[tree.myVectParent[tree.myVectSegments[1].myIndex]].myCoordinate;
+    viewer.addBall(p2,tree.myVectSegments[1].myRadius);
+    viewer << DGtal::CustomColors3D(DGtal::Color(0,250,0),DGtal::Color(0,250,0));
+    viewer.addCylinder(p1,p2,tree.myVectSegments[1].myRadius*thickness);
+    
+    for (auto s : tree.myVectSegments) {
+      // test if the segment is the root or its parent we do not display (already done).
+      if (s.myIndex == 0 || s.myIndex == 1)
+        continue;
+      
+      DGtal::Z3i::RealPoint distal = s.myCoordinate;
+      DGtal::Z3i::RealPoint proxital = tree.myVectSegments[tree.myVectParent[s.myIndex]].myCoordinate;
+      viewer << DGtal::CustomColors3D(DGtal::Color(250,0,0),DGtal::Color(250,0,0));
+      viewer.addBall(distal,tree.myVectSegments[s.myIndex].myRadius);
+      viewer << DGtal::CustomColors3D(DGtal::Color(0,250,0),DGtal::Color(0,250,0));
+      //viewer.addBall(distal,1);
+      viewer.addCylinder(distal,proxital,tree.myVectSegments[s.myIndex].myRadius*thickness);
+      //std::cout<<"r="<<tree.myVectSegments[s.myIndex].myRadius<<std::endl;
+      i++;
+    }
+    /*
+     //Display Sphere domaine
+     viewer << DGtal::CustomColors3D(DGtal::Color(0,0,250,10),DGtal::Color(0,0,250,10));
+     viewer.addBall(DGtal::Z3i::RealPoint(0,0,0),my_rPerf);
+     */
+    viewer<< MyViewer::updateDisplay;
+    return application.exec();
+  }
+#endif
   unsigned int i = 0;
   double thickness = 1;
-  viewer << DGtal::CustomColors3D(DGtal::Color(0,0,250),DGtal::Color(0,0,250));
-  DGtal::Z3i::RealPoint p1 = tree.myVectSegments[1].myCoordinate;
-  DGtal::Z3i::RealPoint p2 = tree.myVectSegments[tree.myVectParent[tree.myVectSegments[1].myIndex]].myCoordinate;
-  viewer.addBall(p2,tree.myVectSegments[1].myRadius);
-  viewer << DGtal::CustomColors3D(DGtal::Color(0,250,0),DGtal::Color(0,250,0));
-  viewer.addCylinder(p1,p2,tree.myVectSegments[1].myRadius*thickness);
-  
+
+  // Export 3D mesh of the tree
+  DGtal::Mesh<DGtal::Z3i::RealPoint> aMesh;
   for (auto s : tree.myVectSegments) {
     // test if the segment is the root or its parent we do not display (already done).
     if (s.myIndex == 0 || s.myIndex == 1)
       continue;
- 
+    
     DGtal::Z3i::RealPoint distal = s.myCoordinate;
     DGtal::Z3i::RealPoint proxital = tree.myVectSegments[tree.myVectParent[s.myIndex]].myCoordinate;
-    viewer << DGtal::CustomColors3D(DGtal::Color(250,0,0),DGtal::Color(250,0,0));
-    viewer.addBall(distal,tree.myVectSegments[s.myIndex].myRadius);
-    viewer << DGtal::CustomColors3D(DGtal::Color(0,250,0),DGtal::Color(0,250,0));
-    //viewer.addBall(distal,1);
-    viewer.addCylinder(distal,proxital,tree.myVectSegments[s.myIndex].myRadius*thickness);
+//    viewer << DGtal::CustomColors3D(DGtal::Color(250,0,0),DGtal::Color(250,0,0));
+//    viewer.addBall(distal,tree.myVectSegments[s.myIndex].myRadius);
+//    viewer << DGtal::CustomColors3D(DGtal::Color(0,250,0),DGtal::Color(0,250,0));
+//    //viewer.addBall(distal,1);
+    auto v = {distal, proxital};
+   // viewer.addCylinder(distal,proxital,tree.myVectSegments[s.myIndex].myRadius*thickness);
+    DGtal::Mesh<DGtal::Z3i::RealPoint>::createTubularMesh(aMesh, v, tree.myVectSegments[s.myIndex].myRadius*thickness, 0.05);
+    DGtal::Mesh<DGtal::Z3i::RealPoint>::cre
     //std::cout<<"r="<<tree.myVectSegments[s.myIndex].myRadius<<std::endl;
     i++;
   }
-  /*
-  //Display Sphere domaine
-  viewer << DGtal::CustomColors3D(DGtal::Color(0,0,250,10),DGtal::Color(0,0,250,10));
-  viewer.addBall(DGtal::Z3i::RealPoint(0,0,0),my_rPerf);
-  */
-  viewer<< MyViewer::updateDisplay;
-  return application.exec();
-
+  aMesh >> outputMeshName;
   return EXIT_SUCCESS;
 }
