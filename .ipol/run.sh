7a1e5c8b766bd57bf78302af45bce74d9aa7efd3
Bertrand Kerautret <bertrand.kerautret@univ-lyon2.fr>
2023-02-08 15:48:50 +0100

    test wait

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -80,8 +80,8 @@ else
   echo "algoDim=3" >> algo_info.txt
    echo "domain=1" >> algo_info.txt
   export DISPLAY=:1; Xvfb "$DISPLAY" -screen 0 1024x768x24 &
-  timeout 2s meshViewer resultVessel.obj &  sleep 1.5; import -window root -display :1 -screen extractVisu.png ; convert extractVisu.png  -crop 800x600+0+0  visuMeshArchive.png
-  timeout 2s meshViewer liver05Domain.obj &  sleep 1.5; import -window root -display :1 -screen tmp.png ; convert tmp.png  -crop 800x600+0+0  visuMeshDomain.png
+  timeout 2s meshViewer resultVessel.obj &  sleep 1.5 &&  import -window root -display :1 -screen extractVisu.png && convert extractVisu.png  -crop 800x600+0+0  visuMeshArchive.png
+  timeout 2s meshViewer liver05Domain.obj &  sleep 1.5 && import -window root -display :1 -screen tmp.png && convert tmp.png  -crop 800x600+0+0  visuMeshDomain.png
 fi
 
 
