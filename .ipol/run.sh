8fda03da091eb41a7cc94142026a6d25ad9bbe2a
Bertrand Kerautret <bertrand.kerautret@univ-lyon2.fr>
2023-03-16 16:56:18 +0100

    demo density res display

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -50,7 +50,7 @@ then
   fi
   echo "algoImp=1" >> algo_info.txt 
   width=480; height=480;
-  COMMANDGem2D3="convert -density 400 -resize ${width}x${height}  -crop ${width}x${height} result.svg result.png"
+  COMMANDGem2D3="convert -density 800 -resize ${width}x${height}  -crop ${width}x${height} result.svg result.png"
   applyCommand COMMANDGem2D1 COMMANDGem2D2 COMMANDGem2D3
   echo "algoDim=2" >> algo_info.txt 
 elif test -f "$INPUT"
