5d28a675cbf85117e44ca60b9047093e3c7323ca
Bertrand Kerautret <bertrand.kerautret@univ-lyon2.fr>
2023-03-16 17:21:39 +0100

    demo density sleep

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -94,7 +94,7 @@ then
   echo "domain=0" >> algo_info.txt
 
   export DISPLAY=:1; Xvfb "$DISPLAY" -screen 0 1024x768x24 &
-  meshViewer result.obj -b 240 240 240 2>&1 |  while read -r line; do if [[ $line == "[done]." ]] ; then sleep 0.5; import -window root -display :1 -screen extractVisu.png ; convert extractVisu.png  -crop 800x600+0+0  visuMeshArchive.png; pkill meshViewer;  fi; done;
+  meshViewer result.obj -b 240 240 240 2>&1 |  while read -r line; do if [[ $line == "[done]." ]] ; then sleep 1; import -window root -display :1 -screen extractVisu.png ; convert extractVisu.png  -crop 800x600+0+0  visuMeshArchive.png; pkill meshViewer;  fi; done;
 
   
 else
