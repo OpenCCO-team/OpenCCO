6b07d839cfd34510e08451b73aeec623e79b7c16
ryacuda <adam.germain1@gmail.com>
2023-09-05 14:54:32 +0200

    fixes after merge

--- bin/generateMultipleTrees3D.cpp
+++ bin/generateMultipleTrees3D.cpp
@@ -38,11 +38,47 @@ void templamaks()
 	ctree.exportTreesDisplays();
 }
 
-int main()
+int main(int argc, char **argv)
 {
-	templa<3>();
+	srand ((int) time(NULL));
 
-	//templamaks();
+	// parse command line using CLI ----------------------------------------------
+	CLI::App app;
+	app.description("Generated a 3D tree using the CCO algorithm. By default it generates a 3D mesh.");
+	int nbTerm {500};
+	double aPerf {20000};
+	double gamma {3.0};
+
+	double minDistanceToBorder {5.0};
+	bool verbose {false};
+	bool display3D {false};
+	std::string nameImgDom {""};
+	std::string outputMeshName {"result.off"};
+	std::string exportDatName {""};
+	std::string exportXMLName {""};
+	std::vector<int> postInitV {-1,-1,-1};
+	bool squaredImplDomain {false};
+
+	app.add_option("-n,--nbTerm,1", nbTerm, "Set the number of terminal segments.", true);
+	app.add_option("-a,--aPerf,2", aPerf, "The value of perfusion volume.", true);
+	app.add_option("-g,--gamma", gamma, "The value of the gamma parameter.", true);
+	app.add_option("--organDomain,-d", nameImgDom, "Define the organ domain using a mask image (organ=255).");
+	app.add_option("-m,--minDistanceToBorder", minDistanceToBorder, "Set the minimal distance to border. Works only with option --organDomain else it has no effect", true);
+	app.add_option("-o,--outputName", outputMeshName, "Output the 3D mesh into OFF format", true);
+	app.add_option("-e,--export", exportDatName, "Output the 3D mesh into text file", true);
+	app.add_option("-x,--exportXML", exportXMLName, "Output the resulting graph as xml file", true);
+	app.add_flag("-s,--squaredDom",squaredImplDomain , "Use a squared implicit domain instead a sphere (is used only without --organDomain)");
+	auto pInit = app.add_option("-p,--posInit", postInitV, "Initial position of root, if not given the position of point is determined from the image center")
+		->expected(3);
+
+
+	#ifdef WITH_VISU3D_QGLVIEWER
+	app.add_flag("--view", display3D, "display 3D view using QGLViewer");
+	#endif
+	app.add_flag("-v,--verbose", verbose);
+	app.get_formatter()->column_width(40);
+	CLI11_PARSE(app, argc, argv);
+	// END parse command line using CLI ----------------------------------------------
 
 	return 0;
 }
